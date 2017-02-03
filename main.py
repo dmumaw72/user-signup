09cce1416ede9212b556479fd17c26137a87c072
Derek Mumaw <dmumaw72@live.com>
2017-02-02 19:59:10 -0600

    User signup form working and complete

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ user_re = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 user_pas = re.compile(r"^.{3,20}$")
 user_email = re.compile(r"^[\S]+@[\S]+.[\S]+$")
 
+
+
 #function to validate username. Returns true if username is valid
 def valid_username(username):
     return user_re.match(username)
@@ -57,13 +59,13 @@ header = """<!DOCTYPE html>
              <h1>Signup</h1>"""
 form = """<form method='post'>
           <label>Username</label>
-            <input type = 'text' name = 'username' value = '%(username)s'/><span class='error'>%(error)s</span></br>
+            <input type = 'text' name = 'username' value = '%(username)s'/><span class='error'>%(user_error)s</span></br>
           <label>Password</label>
-            <input type = 'password' name = 'password' /><span class='error'>%(error)s</span></br>
+            <input type = 'password' name = 'password' /><span class='error'>%(pass_error)s</span></br>
           <label>Verify Password</label>
-            <input type='password' name='verify' /><span class='error'>%(error)s</span></br>
+            <input type='password' name='verify' /><span class='error'>%(match_error)s</span></br>
           <label>Email (Optional)</label>
-            <input type='text' name='email' value = '%(email)s'/><span class='error'>%(error)s</span></br>
+            <input type='text' name='email' value = '%(email)s'/><span class='error'>%(email_error)s</span></br>
           <button>Submit</button>
           </form>
         """
@@ -72,8 +74,11 @@ footer = """</body></html>"""
 
 class MainHandler(webapp2.RequestHandler):
     #write_form function writes the form including an error message if one is passed into the function
-    def write_form(self, error="", username="", email=""):
-        self.response.write(header + form % {"error":error,
+    def write_form(self, user_error="",pass_error="", match_error="", email_error="", username="", email=""):
+        self.response.write(header + form % {"user_error":user_error,
+                                             "pass_error":pass_error,
+                                             "match_error":match_error,
+                                             "email_error":email_error,
                                              "username":username,
                                              "email":email} + footer)
 
@@ -85,6 +90,8 @@ class MainHandler(webapp2.RequestHandler):
         password = self.request.get("password")
         ver_pass = self.request.get("verify")
         email = self.request.get("email")
+        err_string = ""
+
 
         #escaped form variables
         esc_username = cgi.escape(username)
@@ -92,27 +99,49 @@ class MainHandler(webapp2.RequestHandler):
         esc_var_pass = cgi.escape(ver_pass)
         esc_email = cgi.escape(email)
 
+        #Boolean value for errors in form
+        Error = False
+
+        #error message variables for display if there is a form error
+        user_error = ""
+        pass_error = ""
+        match_error = ""
+        email_error = ""
+
+
     #test validity of user input
         if not (username and valid_username(username)):
-            error = "You must enter a valid username"
-            self.write_form(error)
-            #self.redirect("/?error=" + error)
-        #test if there is a password entered and it is valid
-        elif not (password and valid_password(password)):
-            error = "Enter a valid password"
-            self.write_form(error)
-        elif not (pass_match(password, ver_pass)):
-            error = "Passwords do not match"
-            self.write_form(error)
-        elif (email):
-            if not valid_email(email):
-                error = "Enter a valid email address"
-                self.response.write(header + form % {"error": error,
-                                                     "username": username,
-                                                     "email": email} + footer)
-            else:
-                self.redirect("/welcome?username=" + username)
+            user_error = "You must enter a valid username"
+            Error = True
+            err_string += user_error + ","
 
+
+
+        #test if there is a password entered and it is valid
+        if not (password and valid_password(password)):
+            pass_error = "Enter a valid password"
+            Error = True
+            err_string += pass_error + ","
+
+
+
+
+        if not (pass_match(password,ver_pass)):
+            match_error = "Passwords do not match"
+            Error = True
+            err_string += match_error + ","
+
+
+        if email:
+            if not valid_email(email):
+                email_error = "Enter a valid email address"
+                Error = True
+                err_string += email_error
+        #Check if Error variable is true and then redisplay form with errors
+        if Error:
+            self.write_form(user_error, pass_error, match_error, email_error, username, email)
+        else:
+            self.redirect("/welcome?username=" + username)
 
 
 class WelcomeHandler(webapp2.RequestHandler):
